f72fbc48fb39627d80a7e068ecc0bbb3eb31cc5c
Rolly <darkrolly@gmail.com>
2022-07-08 15:30:30 +0200

    Simplifiation du code

--- Ex1.java
+++ Ex1.java
@@ -1,31 +1,26 @@
 import java.util.Scanner;
 
 /**
- * Ex1
- */
-
+* Ex1
+*/
 public class Ex1 {
-static Scanner input=new Scanner(System.in);
-
-    public static void main(String[] args) {
-        String firstName=ChooseFname();
-        String lastName=ChooseLname();
-}
-
-    static String ChooseFname()
-    {
-        System.out.println("Entrez votrre prénom:");
-String choice =input.nextLine();
-choice = choice.substring(0,1).toUpperCase() + choice.substring(1).toLowerCase();
-        System.out.println("Votre prénom est" + choice);
+    static Scanner input=new Scanner(System.in);
+    public static void main(String[] args) 
+{
+        String fname=ChooseFname();
+        String lname=ChooseLname();
+        System.out.println("Vous vous appelez " + fname + " " + lname);        
+    }
+    static String ChooseFname(){
+        System.out.println("Entrez votre prénom:");
+        String choice=input.nextLine();
         return choice;
     }
-    static String ChooseLname()
-    {
-        System.out.println("Entrez votrre nom:");
-String choice =input.nextLine();
-choice = choice.substring(0,1).toUpperCase() + choice.substring(1).toLowerCase();
-        System.out.println("Votre nom est" + choice);
+    
+    static String ChooseLname(){
+        System.out.println("Entrez votre nom:");
+        String choice=input.nextLine();
         return choice;
     }
-   }    
+
+}   
